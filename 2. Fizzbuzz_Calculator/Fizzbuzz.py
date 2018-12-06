8a78d985873bb7ad5899756a1e578f537cdec2a2
Evert Dhoest <evertdhoest@gmail.com>
2018-12-06 12:33:22 +0100

    Fizzbuzz rewritten

--- 2. Fizzbuzz_Calculator/Fizzbuzz.py
+++ 2. Fizzbuzz_Calculator/Fizzbuzz.py
@@ -1,19 +1,19 @@
-first = True
-number = 0
-while number < 1 or number > 100 or type(number) == str:
-    number = input("Choose a number between 1 and 100: ")
-    if number.isdigit():
-        number = int(number)
-        for i in range(1, int(number)):
-            if i % 3 == 0 and i % 5 == 0:
-                print("Fizzbuzz")
-            elif i % 3 == 0:
-                print("Fizz")
-            elif i % 5 == 0:
-                print("Buzz")
-            else:
-                print(i)
-    else:
-        print("Please enter an integer between 1 and 100")
-        break
+number = input("Select a number between 1 and 100: ")
 
+start = 1
+
+while not number.isdigit() or (int(number) < 1 or int(number) > 100):
+    print("This input is incorrect")
+    number = input("Select a number between 1 and 100: ")
+
+number = int(number)
+while start <= number:
+    if (start % 3) and (start % 5) == 0:
+        print("Fizzbuzz")
+    elif (start % 3) == 0:
+        print("Fizz")
+    elif (start % 5) == 0:
+        print("Buzz")
+    else:
+        print(str(start))
+    start += 1
